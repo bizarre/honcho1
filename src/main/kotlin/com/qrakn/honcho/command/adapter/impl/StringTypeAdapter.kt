a453e3abcb8aa8dfc04856be184beeee5425d9f0
Alex <contact@bizarrealex.com>
2019-02-20 16:45:26 -0800

    Change honcho version (1.3-SNAPSHOT), yay!

--- src/main/kotlin/com/qrakn/honcho/command/adapter/impl/StringTypeAdapter.kt
+++ src/main/kotlin/com/qrakn/honcho/command/adapter/impl/StringTypeAdapter.kt
@@ -1,7 +1,6 @@
 package com.qrakn.honcho.command.adapter.impl
 
 import com.qrakn.honcho.command.adapter.CommandTypeAdapter
-import org.bukkit.Bukkit
 
 class StringTypeAdapter : CommandTypeAdapter {
     override fun <T> convert(string: String, type: Class<T>): T {
