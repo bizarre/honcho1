ea3d5f10fe9be6d25a2d9aa93b484eb14cb78505
Ruby <xjaxzee@gmail.com>
2019-02-26 18:30:32 -0500

    Make CommandTypeAdapter's accept null values

--- src/main/kotlin/com/qrakn/honcho/command/adapter/CommandTypeAdapter.kt
+++ src/main/kotlin/com/qrakn/honcho/command/adapter/CommandTypeAdapter.kt
@@ -4,6 +4,6 @@ import org.bukkit.command.CommandSender
 import java.lang.Exception
 
 interface CommandTypeAdapter {
-    fun <T> convert(string: String, type: Class<T>): T
+    fun <T> convert(string: String, type: Class<T>): T?
     fun onException(exception: Exception, sender: CommandSender, input: String): Boolean { return false }
 }
